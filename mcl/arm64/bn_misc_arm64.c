1e00a9352adbc0403c2bbbd2ef1689bfecbe36ca
jangko <jangko128@gmail.com>
2026-01-13 11:08:05 +0700

    use _ prefix for apple inline assembly when accessing C globals

--- mcl/arm64/bn_misc_arm64.c
+++ mcl/arm64/bn_misc_arm64.c
@@ -155,13 +155,13 @@ static void bn_swap_2x4_toBE(uint64_t* r, const _bn_mini_g1* a) {
 
 #if defined(__APPLE__) && defined(__arm64__)
 #define FP_OP_P(a) \
-  "adrp %[" #a "], FP_OP+8@page\n"\
-  "add  %[" #a "], %[" #a "], FP_OP+8@pageoff\n"
+  "adrp %[" #a "], _FP_OP+8@page\n"\
+  "add  %[" #a "], %[" #a "], _FP_OP+8@pageoff\n"
 
 
 #define BN_ZERO_ADDR(a) \
-  "adrp %[" #a "], BN_ZERO@page\n"\
-  "add  %[" #a "], %[" #a "], BN_ZERO@pageoff\n"
+  "adrp %[" #a "], _BN_ZERO@page\n"\
+  "add  %[" #a "], %[" #a "], _BN_ZERO@pageoff\n"
 
 #else
 
